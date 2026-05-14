2d652e8d51ae4a5d26b5e46a3eac0337b21f46df
Irene Ballantini <irene.ballantini@studio.unibo.it>
2026-05-14 12:02:13 +0200

    refactor: implement preprocessing logic in RadiomicsPreprocessor class
    - Added data resampling logic (isotropic and reference-based)
    - Updated docstrings
    - Modified __main__.py to match the new class API

--- nsclc_survival/__main__.py
+++ nsclc_survival/__main__.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from nsclc_survival import __version__
+from __version__ import __version__
+from preprocessing import RadiomicsPreprocessor
+from settings import ORGANIZED_DATA_PATH, PREPROCESSED_DATA_PATH
 
 def main (): 
-    print(__version__)     
+    print(__version__) 
+
+    processor = RadiomicsPreprocessor(ORGANIZED_DATA_PATH, PREPROCESSED_DATA_PATH)    
+
+    processor.process_all_patients()
+
+if __name__ == "__main__":
+    main()
+
+
+
--- nsclc_survival/preprocessing.py
+++ nsclc_survival/preprocessing.py
@@ -1,103 +1,216 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" 
-To convert clinical data in format NIfTI (.nii.gz).
-
-To summarize:
-
-- rt_utils trasforms the vectors (points of the contours) in an array (pixels).
-
-- SimpleITK trasforms the array (pixels) in a spatial volume (voxels) ready to be saved as a .nii.gz file.
-"""
-
 import numpy as np
 import SimpleITK as sitk
 from pathlib import Path
 from rt_utils import RTStructBuilder
-from settings import ORGANIZED_DATA_PATH, PREPROCESSED_DATA_PATH
 
-def numpy_to_itk(mask_np, reference_image):
-    # rt_utils returns (H, W, Slices), SimpleITK works with (Slices, H, W) 
-    # or it requires a reordering. rt_utils is already aligned with the DICOM series.
-    mask_itk = sitk.GetImageFromArray(mask_np.astype(np.uint8).transpose(2, 0, 1))
+class RadiomicsPreprocessor: 
+    """
+    Preprocessor for converting clinical data to NIfTI format (.nii.gz):
+        - Uses rt_utils to convert contour points (vectors) from RTSTRUCT into a binary mask (numpy array with pixels).
+        - Uses SimpleITK to convert the binary mask into a spatial volume (SimpleITK image with voxels) and saves it as .nii.gz.
+        
+    """
+
+    def __init__(self, organized_path, preprocessed_path):
+        self.organized_path = organized_path
+        self.preprocessed_path = preprocessed_path
+        self.preprocessed_path.mkdir(parents=True, exist_ok=True)
+
+    def process_all_patients(self):
+        patient_folders = sorted([f for f in self.organized_path.iterdir() if f.is_dir()])
+        print(f"Found {len(patient_folders)} patients.")
+
+        processed_successfully = []
+
+        for i, patient_dir in enumerate(patient_folders, start=1):
+            patient_id = patient_dir.name
+
+            # Check if file already exists to avoid re-processing
+            output_check = self.preprocessed_path / patient_id / "label.nii.gz"
+            if output_check.exists():
+                print(f"\n[{i}/{len(patient_folders)}] Skipping {patient_id}: Already processed.")
+                continue
+
+            print(f"\nProcessing folder {i}/{len(patient_folders)}: Patient {patient_id}")
+            result = self.process_patient(patient_dir, patient_id)
+
+            if result:
+                print(f"--- SUCCESS: {patient_id} saved correctly.")
+                processed_successfully.append(result)
+            else:
+                print(f"--- FAILED: {patient_id} was skipped.")
+
+        if len(processed_successfully) != 0:
+            print(f"\nPre-processing completed! {len(processed_successfully)}/{len(patient_folders)} patients processed.")
+        else:
+            print("\nPatients were already processed or skipped due to errors. No new files created.")
+
+        print(f"The NIfTI files are in: {self.preprocessed_path}")
     
-    # Copy spatial metadata from the original CT
-    mask_itk.SetSpacing(reference_image.GetSpacing())
-    mask_itk.SetOrigin(reference_image.GetOrigin())
-    mask_itk.SetDirection(reference_image.GetDirection())
-    return mask_itk
+    def process_patient(self, patient_dir, patient_id, roi_target="GTV-1"): 
 
-PREPROCESSED_DATA_PATH.mkdir(parents=True, exist_ok=True)
+        """
+        Process a single patient directory to extract the CT and the tumor mask, and save them in NIfTI format.
 
-# Retrieve patient folders in the organized data direcotry
-patient_folders = sorted([f for f in ORGANIZED_DATA_PATH.iterdir() if f.is_dir()])
-print(f"Found {len(patient_folders)} patients to process.")
+        GTV-1 = label for Primary Gross Tumor Volume
 
-for i, patient_dir in enumerate(patient_folders, start=1):
-    patient_id = patient_dir.name
-    print(f"\nProcessing folder {i}/{len(patient_folders)}: Patient {patient_id}")
+        Args:
+            patient_dir (Path): path to the patient's folder containing the CT and RTSTRUCT subfolders.
+            patient_id (str): ID of the patient (used for naming the output folder).
+            roi_target (str): Name of the ROI to extract. Defaults to "GTV-1".
+
+        Returns:
+            str | None: patient_id if processed successfully, None if failed
+        """
+      
+        path_ct = patient_dir / "CT"
+        #Note: RTSTRUCT file usually has a single file
+        rt_files = list((patient_dir / "RTSTRUCT").glob("*.dcm")) 
+
+        if not rt_files:
+            print(f"Skipping {patient_id}: No file RTSTRUCT found.")
+            return None
         
-    path_ct = patient_dir / "CT"
-    #Note: RTSTRUCT file usually has a single file
-    rt_files = list((patient_dir / "RTSTRUCT").glob("*.dcm")) 
+        # Take the first RTSTRUCT file found
+        path_rtstruct = rt_files[0]  
 
-    if not rt_files:
-        print(f"Skipping {patient_id}: No file RTSTRUCT found.")
-        continue
+        print(f"CT Path: {path_ct}")
+        print(f"RT Path: {path_rtstruct}")
 
-    path_rtstruct = rt_files[0]  # Take the first RTSTRUCT file found
+        try:
+            # Load the RTSTRUCT 
+            rtstruct = RTStructBuilder.create_from(
+                dicom_series_path=str(path_ct), 
+                rt_struct_path=str(path_rtstruct),
+            )
 
-    print(f"CT Path: {path_ct}")
-    print(f"RT Path: {path_rtstruct}")
+            rois = rtstruct.get_roi_names()
+            print(f"ROI found for this patient {patient_id}: {rois}")
 
-    try:
-        rtstruct = RTStructBuilder.create_from(
-            dicom_series_path=str(path_ct), 
-            rt_struct_path=str(path_rtstruct),
-        )
+            if not rois:
+                print("WARNING: No ROI loaded correctly.")
+                return None
 
-        rois = rtstruct.get_roi_names()
-        print(f"ROI found for this patient {patient_id}: {rois}")
-
-        if not rois:
-            print("WARNING: No ROI loaded correctly.")
+            if roi_target not in rois:
+                print(f"Skipping {patient_id}: ROI '{roi_target}' not found. Available ROIs: {rois}")
+                return None
         
-        # GTV-1 = label for Primary Gross Tumor Volume
-        roi_target = "GTV-1" 
+            # Extract the tumor mask 
+            tumor_mask = rtstruct.get_roi_mask_by_name(roi_target)  
 
-        if roi_target not in rois:
-            print(f"Skipping {patient_id}: ROI '{roi_target}' not found. Available ROIs: {rois}")
-            continue
+            # Verify the dimensions of the mask
+            print(f"Shape of the mask: {tumor_mask.shape}") 
+            # Should be (height, width, number_of_slices)
         
-        # Extract the tumor mask 
-        tumor_mask = rtstruct.get_roi_mask_by_name(roi_target)  
+            # Load the CT series as a SimpleITK image
+            reader = sitk.ImageSeriesReader()
+            dicom_names = reader.GetGDCMSeriesFileNames(str(path_ct))
+            reader.SetFileNames(dicom_names)
+            ct_image = reader.Execute()
 
-        # Verify the dimensions of the mask
-        print(f"Shape of the mask: {tumor_mask.shape}") 
-        # Should be (height, width, number_of_slices)
+            # Now the object ct_image "knows" the size of the voxels (e.g., 1mm x 1mm x 3mm)
+            
+            # 1. Transform the tumor mask from numpy array to SimpleITK image
+            tumor_mask_itk = self._numpy_to_itk(tumor_mask, ct_image)
+
+            # 2. Resample CT to isotropic spacing (e.g., 1mm x 1mm x 1mm)
+            print(f"Resampling {patient_id} to 1.0mm isotropic...")
+            ct_resampled = self._resample_image(ct_image, is_label=False)
+
+            # Resample the tumor mask using the ct_resampled
+            mask_resampled = self._resample_mask_to_reference(tumor_mask_itk, ct_resampled)       
+
+            # 3. Saving in NIfTI format
+            output_patient_dir = self.preprocessed_path / patient_id
+            output_patient_dir.mkdir(parents=True, exist_ok=True)
+
+            sitk.WriteImage(ct_resampled, str(output_patient_dir / "image.nii.gz"))
+            sitk.WriteImage(mask_resampled, str(output_patient_dir / "label.nii.gz"))
+
+            return patient_id
+
+        except Exception as e:
+            print(f"Skipping patient {patient_id} due to RTSTRUCT error: {e}")
+            return None
+    
+    def _numpy_to_itk(self, mask_np, reference_image):
+        """ 
+        Convert a numpy array mask (H, W, Slices) to a SimpleITK image (Slices, H, W), ensuring correct spatial metadata.
+
+        Args:
+            mask_np (np.ndarray): Boolean numpy array provided by rt_utils, with shape (H, W, Slices).
+            reference_image (sitk.Image): Original CT image to copy the spatial metadata
+
+        Returns:
+            sitk.Image: Mask as SimpleITK image with correct spatial metadata.
+        """
         
-        # Load the CT series as a SimpleITK image
-        reader = sitk.ImageSeriesReader()
-        dicom_names = reader.GetGDCMSeriesFileNames(str(path_ct))
-        reader.SetFileNames(dicom_names)
-        ct_image = reader.Execute()
+        mask_itk = sitk.GetImageFromArray(mask_np.astype(np.uint8).transpose(2, 0, 1))
+    
+        # Copy spatial metadata from the original CT
+        mask_itk.SetSpacing(reference_image.GetSpacing())
+        mask_itk.SetOrigin(reference_image.GetOrigin())
+        mask_itk.SetDirection(reference_image.GetDirection())
+        return mask_itk
+    
+    def _resample_image(self, itk_image, out_spacing = [1.0, 1.0, 1.0], is_label= False):
+        """
+        Resample a SimpleITK image to a new spacing.
 
-        # Now the object ct_image "knows" the size of the voxels (e.g., 1mm x 1mm x 3mm)
+        Args:
+            itk_image (sitk.Image): Image to resample.
+            out_spacing (list): New spacing in mm. Defaults to [1.0, 1.0, 1.0].
+            is_label (bool):If True, use NearestNeighbor to preserve binary values.
+                            If False, use BSpline to maintain image quality. 
+                            Defaults to False.
 
-        tumor_mask_itk = numpy_to_itk(tumor_mask, ct_image)
+        Returns:
+            sitk.Image: Resampled image.
+        """
+
+        original_spacing = itk_image.GetSpacing()
+        original_size = itk_image.GetSize()
         
-        # Saving in NIfTI format
-        output_patient_dir = PREPROCESSED_DATA_PATH / patient_id
-        output_patient_dir.mkdir(parents=True, exist_ok=True)
+        # Calculate the new size in pixels (Size) to maintain the same physical volume
+        out_size = [
+            int(round(original_size[i] * (original_spacing[i] / out_spacing[i])))
+            for i in range(3)
+        ]
+        
+        resample = sitk.ResampleImageFilter()
+        resample.SetOutputSpacing(out_spacing)
+        resample.SetSize(out_size)
+        resample.SetOutputDirection(itk_image.GetDirection())
+        resample.SetOutputOrigin(itk_image.GetOrigin())
+        resample.SetTransform(sitk.Transform())
+        
+        # Choose the interpolation method
+        if is_label:
+            # For the binary mask: we don't want average values (e.g., 0.5), only 0 or 1
+            resample.SetInterpolator(sitk.sitkNearestNeighbor)
+        else:
+            # For the CT: BSpline ensures better image quality after resampling
+            resample.SetInterpolator(sitk.sitkBSpline)
+            
+        return resample.Execute(itk_image)
+    
+    def _resample_mask_to_reference(self, mask_itk, reference_itk):
+        """
+        Resample the mask to match the spatial properties of the reference image (CT)
 
-        sitk.WriteImage(ct_image, str(output_patient_dir / "image.nii.gz"))
-        sitk.WriteImage(tumor_mask_itk, str(output_patient_dir / "label.nii.gz"))
+        Args:
+            mask_itk (sitk.Image): Mask (label) to resample.
+            reference_itk (sitk.Image): Resampled image (CT) to use as reference.
 
-        print(f"Completed: {patient_id} (CT + Mask saved)")
-
-    except Exception as e:
-        print(f"Skipping patient {patient_id} due to RTSTRUCT error: {e}")
-    continue
-
-print(f"\nPreprocessing completed! The NIfTI files are in: {PREPROCESSED_DATA_PATH}")
+        Returns:
+            sitk.Image: Resampled mask.
+        """
+        resample = sitk.ResampleImageFilter()
+        resample.SetReferenceImage(reference_itk) 
+        resample.SetInterpolator(sitk.sitkNearestNeighbor)
+        resample.SetTransform(sitk.Transform())
+    
+        return resample.Execute(mask_itk)
