ab98c3065bb58a4e74dc18f8cceb6648149111a7
Irene Ballantini <irene.ballantini@studio.unibo.it>
2026-05-19 15:16:18 +0200

    docs: setup initial Sphinx documentation structure

---
+++ docs/source/conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'nsclc_survival'
+copyright = '2026, Irene Ballantini'
+author = 'Irene Ballantini'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon', 
+    'sphinx_rtd_theme',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    #'rst2pdf.pdfbuilder',
+    'nbsphinx',
+    'IPython.sphinxext.ipython_console_highlighting'
+]
+
+templates_path = []
+exclude_patterns = ['build', '**.ipynb_checkpoints']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for PDF output --------------------------------------------------
+
+# Grouping the document tree into LaTeX files. List of tuples# (source start file, target name, title, author, documentclass [howto/manual]).
+latex_engine = 'xelatex'
+latex_documents = [('index', u'nsclc_survival.tex', u'nsclc_survival - Survival Time prediction using CT-extracted features and clinical data', u'Irene Ballantini', 'manual'),]
+latex_show_pagerefs = True
+latex_domain_indices = False
+
+pdf_documents = [('index', u'nsclc_survival', u'nsclc_survival - Survival Time prediction using CT-extracted features and clinical data', u'Irene Ballantini'),]
+
+nbsphinx_input_prompt = 'In [%s]:'
+nbsphinx_kernel_name = 'python3'
+nbsphinx_output_prompt = 'Out[%s]:'
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. nsclc_survival documentation master file, created by
+   sphinx-quickstart on Tue May 19 10:38:10 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+nsclc_survival documentation
+============================
+
+Welcome to the documentation for the **nsclc_survival** project! 
+
+The project focuses on predicting the survival of patients with non-small cell lung cancer (NSCLC) using machine learning techniques. 
+This documentation provides an overview of the project, its structure, and how to use it effectively.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
